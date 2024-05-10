5188c1846dfe898a57cb3cf59b6463f511c90688
TOConde <tomy_t.c@hotmail.com>
2024-05-10 13:56:46 -0300

    Vinculacion BackFront

--- proyecto-backend/src/app.module.ts
+++ proyecto-backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestController } from './controllers/Test.controller';
+import { TestService } from './services/test.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TestController],
+  providers: [AppService, TestService],
 })
 export class AppModule {}
---
+++ proyecto-backend/src/services/test.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+
+export class TestService {
+
+  getTest(): string {
+    const text = 'Esto es un test'
+    return text;
+  }
+}
