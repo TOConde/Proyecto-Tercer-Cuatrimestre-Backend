d39ee76ec4a2072f94f120d0e2cf68e230889a7d
ZulaicaE <zulaicaemiliano@gmail.com>
2024-05-13 14:51:59 -0300

    JwtService y bcrypt para login

--- proyecto-backend/src/app.module.ts
+++ proyecto-backend/src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestController } from './controllers/Test.controller';
 import { TestService } from './services/test.service';
+import { JwtModule } from '@nestjs/jwt';
+import { LoginController } from './controllers/login.controller';
+import { LoginService } from './services/login.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TestController],
-  providers: [AppService, TestService],
+  imports: [
+    JwtModule.register({
+      secret: 'ClaveUltraSecreta',
+      signOptions: {expiresIn: '1h'},
+    }),
+  ],
+  controllers: [AppController, TestController, LoginController],
+  providers: [AppService, TestService, LoginService],
 })
 export class AppModule {}
---
+++ proyecto-backend/src/controllers/login.controller.ts
@@ -0,0 +1,24 @@
+import {
+    Controller,
+    Get, 
+    Post,
+    Body, 
+    HttpException,
+    HttpStatus
+  } from '@nestjs/common';
+  import { LoginService } from 'src/services/login.service';
+  
+  
+@Controller('/auth')
+export class LoginController {
+  constructor(private loginService: LoginService) {}
+
+  @Post('/login')
+    async login(@Body() body: { username: string; password: string }) {
+        const user = this.loginService.validateUser(body.username, body.password);
+        if (!user) {
+            throw new HttpException('NO AUTORIZADO', HttpStatus.UNAUTHORIZED);
+        }
+        return this.loginService.login(user)
+    }
+}
