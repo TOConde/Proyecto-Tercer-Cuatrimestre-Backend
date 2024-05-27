a00412b7cff583f127994417a0a37f22403b113e
TOConde <tomy_t.c@hotmail.com>
2024-05-27 16:34:03 -0300

    providers elimando

--- proyecto-backend/src/usuario/usuario.module.ts
+++ proyecto-backend/src/usuario/usuario.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from 'src/common/common.module';
 import { UsuarioController } from './usuario.controller';
-import { UsuarioService } from './usuario.service';
 
 @Module({
     imports: [CommonModule],
     controllers: [UsuarioController],
-    providers: [UsuarioService],
+    providers: [],
 })
 export class UsuarioModule {}
