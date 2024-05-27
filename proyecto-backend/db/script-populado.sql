25db036b29b3b11a56f3c79684242b0d67e83c7c
TOConde <tomy_t.c@hotmail.com>
2024-05-27 16:57:01 -0300

    agregado un insert user

--- proyecto-backend/db/script-populado.sql
+++ proyecto-backend/db/script-populado.sql
@@ -1,4 +1,5 @@
 insert into roles (codigo, nombre) values ('ADM', 'Administrador');
 insert into roles (codigo, nombre) values ('USR', 'Usuario');
 
-insert into usuarios (email, password, activo, rolID) values ('flixoramaADM@flix.com', 'admin', 1, 1);
+insert into usuarios (email, password, activo, rolID) values ('flixoramaADM@flix.com', '$2a$08$W59jWcwio1TiLx4A8iRyTO1wuDadbtY2pxU5q18RDi6yNWAYyJqXS', 1, 1); /* admin */
+insert into usuarios (email, password, activo, rolID) values ('user1@user.com', '$2a$08$W59jWcwio1TiLx4A8iRyTORC812pGR2a3LhlreiFYWLwkDdjEdHxq', 1, 2);/* 123456 */
