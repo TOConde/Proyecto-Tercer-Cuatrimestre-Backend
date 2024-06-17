d93cf73ec707c78f78ca6fb5fa9a42cb03e30017
TOConde <tomy_t.c@hotmail.com>
2024-06-16 22:06:39 -0300

    agregado de controler y service a .module

--- proyecto-backend/src/administrador/administrador.module.ts
+++ proyecto-backend/src/administrador/administrador.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ImageController } from './controllers/image.controller';
 import { ImageService } from './services/image.service';
 import { CommonModule } from 'src/common/common.module';
+import { AgregarPeliculaService } from './services/agregarPelicula.service';
+import { AgregarPeliculaController } from './controllers/agregarPelicula.controller';
 
 @Module({
     imports: [CommonModule],
-    controllers: [ImageController],
-    providers: [ImageService]
+    controllers: [ImageController, AgregarPeliculaController],
+    providers: [ImageService, AgregarPeliculaService]
 })
 export class AdministradorModule {}
