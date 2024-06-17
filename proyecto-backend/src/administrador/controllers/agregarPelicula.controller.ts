2a8e0e6d5415dc6ecb099a8664fa04b021e2578a
TOConde <tomy_t.c@hotmail.com>
2024-06-16 22:07:10 -0300

    agregado manejo errores

--- proyecto-backend/src/administrador/controllers/agregarPelicula.controller.ts
+++ proyecto-backend/src/administrador/controllers/agregarPelicula.controller.ts
@@ -1,22 +1,35 @@
-import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AgregarPeliculaService } from '../services/agregarPelicula.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('/pelicula')
-export class RegisterController {
+export class AgregarPeliculaController {
   constructor(private agregarPeliculaService: AgregarPeliculaService) { }
 
 
   @Post('/upload')
   @UseInterceptors(FileInterceptor('img'))
   async agregarPelicula(
-    @UploadedFile() file: Express.Multer.File, @Body() body: { titulo: string, sinopsis: string }) {
+    @UploadedFile() file: Express.Multer.File,
+    @Body() body: { titulo: string, sinopsis: string }
+  ): Promise<{ message: string }> {
+    if (!file) {
+      throw new BadRequestException('Inserte una imagen');
+    }
+    if (!body.titulo || !body.sinopsis) {
+      throw new BadRequestException('Inserte un titulo y sinopsis');
+    }
+
     const pelicula = {
       titulo: body.titulo,
       sinopsis: body.sinopsis,
-      img: file
+      img: file,
     };
-    const result = await this.agregarPeliculaService.agregarPelicula(pelicula);
-    return { message: `${result} se ha agregado con éxito!` };
+    try {
+      const result = await this.agregarPeliculaService.agregarPelicula(pelicula);
+      return { message: `${result} se ha agregado con éxito!` };
+    } catch (error) {
+      throw new BadRequestException('Error al agregar la pelicula')
+    }
   }
 }
