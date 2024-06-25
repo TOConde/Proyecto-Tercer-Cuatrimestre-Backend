451b60985e02edab4b4b9ef538978c19c1ef20a9
TOConde <tomy_t.c@hotmail.com>
2024-06-25 20:41:39 -0300

    queries changes

--- proyecto-backend/src/common/queries/common.queries.ts
+++ proyecto-backend/src/common/queries/common.queries.ts
@@ -1,5 +1,5 @@
 const commonQueries = {
-  selectAllMovies: 'select * from peliculas;'
+  selectAllMovies: 'select peliculaID, titulo, sinopsis, fechaEstreno, duracion, urlVideo, url_image, url_image_delete, display_url_image from peliculas;',
 }
 
 export default commonQueries;
