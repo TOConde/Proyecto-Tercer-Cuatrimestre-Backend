9e08e4ce8c8e9409ec67e0b5a58b2f57a058a4d1
TOConde <tomy_t.c@hotmail.com>
2024-06-25 14:45:34 -0300

    agregado video url

--- proyecto-backend/src/administrador/queries/admin.queries.ts
+++ proyecto-backend/src/administrador/queries/admin.queries.ts
@@ -1,5 +1,5 @@
 const adminQueries = {
-    agregarPelicula: 'insert into peliculas (titulo, sinopsis, duracion, fechaEstreno, url_image, url_image_delete, display_url_image) values (?, ?, ?, ?, ?, ?, ?)',
+    agregarPelicula: 'insert into peliculas (titulo, sinopsis, duracion, fechaEstreno, urlVideo, url_image, url_image_delete, display_url_image) values (?, ?, ?, ?, ?, ?, ?, ?)',
     agregarGeneroPelicula: 'insert into pelicula_generos (peliculaID, generoID) values (?, ?)',
 
     selectAllGeneros: 'select * from generos;'
