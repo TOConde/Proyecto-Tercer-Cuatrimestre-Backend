ad003778491d58a56c1df6feb3b702d87f1773e3
TOConde <tomy_t.c@hotmail.com>
2024-07-10 18:38:13 -0300

    cambio de queries

--- proyecto-backend/src/administrador/queries/admin.queries.ts
+++ proyecto-backend/src/administrador/queries/admin.queries.ts
@@ -1,7 +1,6 @@
 const adminQueries = {
     agregarPelicula: 'insert into peliculas (titulo, sinopsis, duracion, fechaEstreno, urlVideo, url_image, url_image_delete, display_url_image, activo) values (?, ?, ?, ?, ?, ?, ?, ?, ?)',
     agregarGeneroPelicula: 'insert into pelicula_generos (peliculaID, generoID) values (?, ?)',
-    eliminarGeneroPelicula: 'DELETE FROM pelicula_generos WHERE peliculaID = ? AND generoID NOT IN (?)',
 
     selectAllGeneros: 'select * from generos;',
     selectGenerosById: 'SELECT p.peliculaID, p.titulo, g.generoID, g.nombreGenero FROM peliculas p JOIN pelicula_generos pg ON p.peliculaID = pg.peliculaID JOIN generos g ON pg.generoID = g.generoID WHERE p.peliculaID = ?;'
