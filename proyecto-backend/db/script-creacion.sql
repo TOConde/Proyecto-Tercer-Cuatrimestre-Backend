e99948a8fa6d9adf8be6d9d74ee28cdaaa4b173a
ZulaicaE <zulaicaemiliano@gmail.com>
2024-07-14 19:43:13 -0300

    fixTablaUsuarios

--- proyecto-backend/db/script-creacion.sql
+++ proyecto-backend/db/script-creacion.sql
@@ -21,6 +21,7 @@ create table if not exists usuarios (
 	idioma VARCHAR(50),
 	fechaDeSuscripcion DATE,
 	tipoDeSuscripcion tinyint,
+    recibirCorreos BOOLEAN DEFAULT true,
 	url_userImage VARCHAR(255),
 	ulr_userBanner VARCHAR(255),
 	primary key (usuarioID),
